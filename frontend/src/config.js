5d9c998ac33f094fc973806d4995ec69373e5d1a
Ashok <ashokgupta2678@gmail.com>
2025-02-16 00:22:17 +0530

    added the docker compose file and also added added way to get env variable for backend url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,6 @@
 // so now it will export the url of the backend here 
-export const Backend_URL = "http://localhost:3000/api/v1";
-// replace with your docker or conaier name 
+export const Backend_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api/v1";
+// export const Backend_URL = "http://localhost:3000/api/v1";
+
+// when the docker conatiner will run it will pickup the enviroment varialbe like that to it's own container name 
+// import.meta.env this is how vite import the url 
