44155588116ff05d0f5d32200d7b6946ca3f5d34
Ashok <ashokgupta2678@gmail.com>
2025-04-14 02:27:59 +0530

    changed the port mapping

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,8 @@ app.use(cors());
 app.use(express.json()); // Ensure this line is before your routes so before the main route 
 app.use("/api/v1", mainRouter); // routing all of it to the routes folder 
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
